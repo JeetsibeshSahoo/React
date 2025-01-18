9d01f662c4a8eb7048fa04a3844911ba7970cbbb
JeetsibeshSahoo <jeetsibes05@gmail.com>
2025-01-18 15:14:27 +0530

    on file

--- 12megaBlog/src/appwrite/config.js
+++ 12megaBlog/src/appwrite/config.js
@@ -20,7 +20,7 @@ export class Service{
             return await this.databases.createDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
-                slug,
+                slug,   // DOCUMENT ID - whatever we give to slug this will treat as document id
                 {
                     title,
                     content,
@@ -32,6 +32,99 @@ export class Service{
         } catch (error) {
             console.log("Appwrite service :: createPost :: error", error);
         }
+    }
+
+    async updatePost (slug, {title, content, featuredImage, status}) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: updatePost :: error", error)
+        }
+    }
+
+    async deletePost (slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error", error)
+            return false
+        }
+    }
+
+    async getPost (slug) {     // this getPost is for single post
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error", error)
+            return false
+        }
+    }
+
+    async getPosts (queries = [Query.equal("status","active")]) {      // this  getPosts is for all post
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error", error)
+            return false
+        }
+    }
+
+    // file upload service
+
+    async uploadFile (file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error", error)
+            return false
+        }
+    }
+
+    async deleteFile (fileId) {
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error", error)
+            return false
+        }
+    }
+
+    getFilePreview (fileId) {
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
     }
 };
 
